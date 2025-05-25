d2a900e854464ad73b1b2474354d329451c44cee
Khanh Tran <ankhanhtran02@gmail.com>
2025-05-25 19:51:52 +0700

    Fix run_uie_lora

--- pseudo_data/src/run_uie_lora.py
+++ pseudo_data/src/run_uie_lora.py
@@ -59,6 +59,7 @@ os.environ['WANDB_DISABLED'] = "True"
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 logger = logging.getLogger(__name__)
 CURRENT_DIR = os.path.dirname(__file__)
+print(f"Current directory: {CURRENT_DIR}")
 
 try:
     nltk.data.find("tokenizers/punkt")
